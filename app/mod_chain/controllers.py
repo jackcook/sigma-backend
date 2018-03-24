d1ae15052a9089fb1df8de9b6cf3c8806f859e79
Jack Cook <hello@jackcook.nyc>
2018-03-24 10:17:59 -0400

    Added endpoints for debugging the blockchain

--- app/mod_chain/controllers.py
+++ app/mod_chain/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 # Import blockchain from application
 from app import blockchain
@@ -13,3 +13,29 @@ def full_chain():
 
     # Return all blockchain data
     return jsonify(blockchain.chain)
+
+@mod_chain.route("/nodes", methods=["POST"])
+def add_node():
+    # Register node that was passed in request body
+    node = request.get_json()["node"]
+    blockchain.register_node(node)
+
+    # Return all blockchain nodes
+    return jsonify(list(blockchain.nodes)), 201
+
+@mod_chain.route("/resolve", methods=["GET"])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            "message": "Our chain was replaced",
+            "new_chain": blockchain.chain
+        }
+    else:
+        response = {
+            "message": "Our chain is authoritative",
+            "chain": blockchain.chain
+        }
+
+    return jsonify(response)
--- run.py
+++ run.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     application.run(
         debug=True,
         host="0.0.0.0",
-        port=5001
+        port=8000
     )
